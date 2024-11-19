6555c3d7356fa1da3433b62c492aacca3b2d214d
Rh4ndon <daverhandon@gmail.com>
2024-11-19 17:00:47 +0800

    rename a folder

---
+++ projects/sms_atms/view/js/top.js
@@ -0,0 +1,11 @@
+if(!localStorage.getItem('userSession')){
+    window.location.href = "login.html";
+  }
+  
+  function logout(){
+    localStorage.removeItem('userSession');
+    window.location.href = "login.html";
+  }
+
+
+  
